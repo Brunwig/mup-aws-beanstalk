3e0fbbe59cb913cc92ff1388891b294bdd3318c1
tcastelli <toni@lytesoft.com>
2025-02-26 00:05:46 +0100

    try fixing deployment of custom node image with immutable deployments

--- src/assets/node.sh
+++ src/assets/node.sh
@@ -17,6 +17,10 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | b
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 if [[ $MAJOR_NODE_VERSION == "14" && $MINOR_NODE_VERSION -ge 21 && $PATCH_NODE_VERSION -ge 4 ]]; then
+  #Install latests 14 version so the folder structure is there when trying to download custom node version
+  nvm install 14
+
+  # Install Meteor's custom NodeJS v14 LTS version
   NODE_INSTALL_PATH="/.nvm/versions/node/v$NODE_VERSION"
 
   if [ -d $NODE_INSTALL_PATH ]; then
@@ -26,7 +30,8 @@ if [[ $MAJOR_NODE_VERSION == "14" && $MINOR_NODE_VERSION -ge 21 && $PATCH_NODE_V
 
     # https://hub.docker.com/layers/meteor/node/14.21.4/images/sha256-f4e19b4169ff617118f78866c2ffe392a7ef44d4e30f2f9fc31eef2c35ceebf3?context=explore
     curl "https://static.meteor.com/dev-bundle-node-os/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz" | tar xzf - -C /tmp/
-    mv /tmp/node-v$NODE_VERSION-linux-x64 $NODE_INSTALL_PATH
+    mv /tmp/node-v$NODE_VERSION-linux-x64 "$NODE_INSTALL_PATH"
+    ls -l $NODE_INSTALL_PATH
   fi
 else
   echo "Using NVM"
